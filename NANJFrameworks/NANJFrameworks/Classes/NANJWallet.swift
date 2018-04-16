aaa74f84c31c959d523df4835d9f1f01a0f8cadb
Nguyen Ba Linh <balinh10t1@gmail.com>
2018-04-17 00:11:02 +0700

    - comment function on WalletManager and NAJIWallet

--- NANJFrameworks/NANJFrameworks/Classes/NANJWallet.swift
+++ NANJFrameworks/NANJFrameworks/Classes/NANJWallet.swift
@@ -25,53 +25,103 @@ public class NANJWallet: NSObject, NANJQRCodeDelegate, NANJNFCDelegate {
     
     private var privateKey: String! = ""
     
+    
+    /// Send NAJI coins to other wallet
+    ///
+    /// - Parameters:
+    ///   - address: wallet target
+    ///   - amouth: the number of coin you want to transfer
     public func sendNANJ(with address: String, amouth: Double) {
         //return by delegate
         
     }
     
+    
+    /// Send NAJI coins to other wallet that you just scaned QR code
+    ///
+    /// - Parameters:
+    ///   - amouth: the number of coin you want to transfer
+    ///   - controller: your view controller
     public func sendNANJWithQRCode(amouth: Double, fromController controller: UIViewController) {
         //return by delegate
         
     }
     
+    
+    /// Send NAJI coins to other wallet that you connected by NFC method
+    ///
+    /// - Parameters:
+    ///   - amouth: the number of coin you want to transfer
+    ///   - controller: your view controller
     public func sendNANJWithNFC(amouth: Double, fromController controller: UIViewController) {
         //return by delegate
         
     }
     
+    
+    /// Get your NAJI amount
+    ///
+    /// - Returns: your NAJI coins
     public func getAmountNANJ() -> Double {
         return self.amountNANJ
     }
     
+    
+    /// Get your ETH amount
+    ///
+    /// - Returns: your ETH coins
     public func getAmountETH() -> Double {
         return self.amountETH
     }
     
+    
+    /// Edit your wallet name. It only store in your device.
+    ///
+    /// - Parameter name: a new name you want.
+    /// - Returns: if edit successful return true and opposite.
     public func editName(name: String) -> Bool{
         return false
     }
     
+    
+    /// After get transactions on async function
+    /// return list transactions in callback delegate.
     public func getTransactionList() {
         //Return by delegate
         //Ex
         self.delegate?.didGetTransactionList(transactions: nil)
     }
     
+    
+    /// <#Description#>
+    ///
+    /// - Returns: <#return value description#>
     public func getURLOnEtherscan() -> URL? {
         
         return nil
     }
     
     //MARK - NANJQRCodeDelegate, NANJNFCDelegate
+    
+    
+    /// <#Description#>
+    ///
+    /// - Parameter address: <#address description#>
     func didScanQRCode(address: String) {
         //Received address from QRCode
     }
     
+    
+    
+    /// <#Description#>
+    ///
+    /// - Parameter address: <#address description#>
     func didScanNFC(address: String) {
         //Received address from NFC
     }
     
+    
+    /// <#Description#>
     func didCloseScan() {
         
     }
--- NANJFrameworks/NANJFrameworks/Classes/NANJWalletManager.swift
+++ NANJFrameworks/NANJFrameworks/Classes/NANJWalletManager.swift
@@ -26,20 +26,18 @@ public class NANJWalletManager: NSObject {
      
      - returns: Wallet by NANJWalletManagerDelegate.
      */
+    
     public func createWallet() {
         //Return value with delegate
         //Ex
         self.delegate?.didCreateWallet(wallet: nil, error: nil)
     }
     
-    /**
-     Import wallet.
-     
-     - parameter private: Private key of wallet.
-     - parameter json: Keystore of wallet
-     
-     - returns: Wallet by NANJWalletManagerDelegate.
-     */
+    /// Import a wallet with private key and json string
+    ///
+    /// - Parameters:
+    ///   - private: Private key of wallet.
+    ///   - json: Keystore of wallet
     public func importWallet(with private: String?, with json: NSDictionary) {
         //Return value with delegate
         
@@ -53,33 +51,58 @@ public class NANJWalletManager: NSObject {
      
      - returns: Wallet by NANJWalletManagerDelegate.
      */
+    
+    
+    /// Remove wallet.
+    ///
+    /// - Parameter wallet: that you want to remove
+    /// - Returns: if remove successful return true and opposite
     public func removeWallet(wallet: NANJWallet) -> Bool {
         
         return false
     }
     
+    
+    /// Enable wallet
+    ///
+    /// - Parameter wallet: that you want to enable
+    /// - Returns: if enable successful return true and opposite
     public func enableWallet(wallet: NANJWallet) -> Bool {
         
         return false
     }
     
+    
+    /// Return current wallet that is enabled
+    ///
+    /// - Returns: return current NANJWallet
     public func getCurrentWallet() -> NANJWallet? {
         
         return nil
     }
     
+    
+    /// After finish get wallets on async function
+    /// It return list wallets in callback delegate.
     public func getWalletList() {
-        //Return value with delegate
+        
         
     }
     
+    
+    /// After finish get a wallet from QR code on async function
+    /// It return a NANJWallet in callback delegate.
     public func getWalletFromQRCode() {
-        //Return value with delegate
+        
         
     }
     
+    
+    /// Return value with delegate
+    /// After finish get Rate of NAJI on async function
+    /// It return a NANJ Rate in callback delegate.
     public func getNANJRate(){
-        //Return value with delegate
+        
 
     }
 
