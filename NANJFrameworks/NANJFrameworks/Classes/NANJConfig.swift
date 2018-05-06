2d977ae2a666b06a8eb14dc751dd685f1cb5d296
AnhKu <vanlong10t1@gmail.com>
2018-05-06 20:35:56 +0700

    SDK - Add NANJConfig

---
+++ NANJFrameworks/NANJFrameworks/Classes/NANJConfig.swift
@@ -0,0 +1,20 @@
+//
+//  NANJConfig.swift
+//  NANJFrameworks
+//
+//  Created by Long Lee on 5/6/18.
+//
+
+import Foundation
+
+struct NANJConfig {
+    static let rpcServer: RPCServer = RPCServer.rinkeby //Test net.
+}
+
+struct NANJContract {
+    static let contract: String = "0x39d575711BBCa97D554f57801de3090aFE74dC12"
+    static let address: String = "0x39d575711BBCa97D554f57801de3090aFE74dC12"
+    static let name: String = "ESNJCOIN"
+    static let symbol: String = "ESNJ"
+    static let decimals: Int = 8
+}
