94bf5d90407e50916ac6f92d77684412de075a93
AnhKu <vanlong10t1@gmail.com>
2018-05-07 00:20:52 +0700

    Example - implement get wallet amount.

--- NANJFrameworks/Example/NANJFrameworks/WalletViewController.swift
+++ NANJFrameworks/Example/NANJFrameworks/WalletViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 import NANJFrameworks
 import SDWebImage
 
-class WalletViewController: BaseViewController, NANJWalletManagerDelegate {
+class WalletViewController: BaseViewController, NANJWalletManagerDelegate, NANJWalletDelegate {
     
     @IBOutlet weak var imvQRCode: UIImageView!
     @IBOutlet weak var lblAddress: UILabel!
     @IBOutlet weak var lblCoin: UILabel!
+    @IBOutlet weak var lblCoinETH: UILabel!
+    
+    fileprivate var currentWallet: NANJWallet?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +37,9 @@ class WalletViewController: BaseViewController, NANJWalletManagerDelegate {
     
     
     fileprivate func loadCurrentWallet() {
-        if let wallet = NANJWalletManager.shared.getCurrentWallet() {
+        self.currentWallet = NANJWalletManager.shared.getCurrentWallet()
+        self.currentWallet?.delegate = self
+        if let wallet = self.currentWallet {
             self.lblAddress.text = wallet.address
             self.imvQRCode.backgroundColor = UIColor.white
             self.imvQRCode.sd_setImage(with: URL(string: String(format: "http://api.qrserver.com/v1/create-qr-code/?data=%@", wallet.address)), completed: nil)
@@ -46,7 +51,29 @@ class WalletViewController: BaseViewController, NANJWalletManagerDelegate {
     }
     
     fileprivate func loadBalance() {
-        NANJWalletManager.shared.getCurrentWallet()?.getAmountNANJ()
+        self.currentWallet?.getAmountNANJ()
+        self.currentWallet?.getAmountETH()
     }
     
+    
+    //MARK: - NANJWalletManagerDelegate
+
+    
+    
+    //MARK: = NANJWalletDelegate
+    func didSendNANJCompleted(transaction: NANJTransaction?) {
+        
+    }
+    
+    func didGetTransactionList(transactions: Array<NANJTransaction>?) {
+        
+    }
+    
+    func didGetAmountNANJ(wallet: NANJWallet, amount: String, error: Error?) {
+        self.lblCoin.text = String(format: "%@ (ESNJCOIN)", amount)
+    }
+    
+    func didGetAmountETH(wallet: NANJWallet, amount: String, error: Error?) {
+        self.lblCoinETH.text = String(format: "%@ (ETH)", amount)
+    }
 }
