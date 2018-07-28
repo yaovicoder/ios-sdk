1c9691c77830a1f95ebce6eb6c35cdc533818017
Tuan Nguyen <tuan@enablestartup.com>
2018-07-28 22:20:01 +0700

    Add nonce request to allow wallet to send mutiple meta-tx

--- NANJFrameworks/Classes/NANJWallet.swift
+++ NANJFrameworks/Classes/NANJWallet.swift
@@ -134,90 +134,80 @@ public class NANJWallet: NSObject {
         //  CREATE TX_HASH INPUT WITH TX_RELAY, WALLET OWNER,
         //              PAD, NANJCOIN ADDRESS
         print("* * * * * * * * * * * * STEP 2 * * * * * * * * * * * *")
-        let encoderNonce = ABIEncoder()
-        let functionNonce = Function(name: "getNonce", parameters: [.address])
-        try! encoderNonce.encode(function: functionNonce, arguments: [addressETH])
-        let sendNonce = encoderNonce.data
         
-        let requestNonce = EtherServiceRequest(
-            batch: BatchFactory().create(CallRequest(to: NANJConfig.TX_RELAY_ADDRESS, data: sendNonce.hexEncoded))
-        )
-        
-        Session.send(requestNonce) { result in
-            switch result {
-            case .success(let count):
-//                let valueNonce = BigUInt(Data(hex: count))
-                let pad = count.drop0x //valueNonce.description.paddingStart(64, with: "0")
-                let txHashInput = String(format: "0x1900%@%@%@%@%@",
-                                         NANJConfig.TX_RELAY_ADDRESS.drop0x,
-                                         NANJConfig.WALLET_OWNER.drop0x,
-                                         pad,
-                                         NANJConfig.META_NANJCOIN_MANAGER.drop0x,
-                                         forwardEncoded.hexEncoded.drop0x
-                )
-                print("Hash forward: ", forwardEncoded.hexEncoded)
-                print("Hash Input: ", txHashInput)
-                print("\n")
-                
-                // SIGN TX_HASH_INPUT
-                print("* * * * * * * * * * * * STEP 2 * * * * * * * * * * * *")
-                let __bytesSign = Array<UInt8>(hex: txHashInput)
-                let __hashSign = Digest.sha3(__bytesSign, variant: .keccak256)
-                let __txHashSignData = EtherKeystore.shared.signHash(Data(bytes: __hashSign), for: currentAccount)
-                guard let __txHashSign = __txHashSignData.value  else {
-                    self.delegate?.didSendNANJError!(error: "Sign data error")
-                    return
+        self.retrieveRelayNonce { (relayNonce, error) in
+            if let nonceError =  error  {
+                self.delegate?.didSendNANJError?(error: nonceError.localizedDescription)
+                return
+            }
+            
+            let pad = relayNonce.serialize().toHexString().paddingStart(64, with: "0")
+            
+            let txHashInput = String(format: "0x1900%@%@%@%@%@",
+                                     NANJConfig.TX_RELAY_ADDRESS.drop0x,
+                                     NANJConfig.WALLET_OWNER.drop0x,
+                                     pad,
+                                     NANJConfig.META_NANJCOIN_MANAGER.drop0x,
+                                     forwardEncoded.hexEncoded.drop0x
+            )
+            print("Hash forward: ", forwardEncoded.hexEncoded)
+            print("Hash Input: ", txHashInput)
+            print("\n")
+            
+            // SIGN TX_HASH_INPUT
+            print("* * * * * * * * * * * * STEP 2 * * * * * * * * * * * *")
+            let __bytesSign = Array<UInt8>(hex: txHashInput)
+            let __hashSign = Digest.sha3(__bytesSign, variant: .keccak256)
+            let __txHashSignData = EtherKeystore.shared.signHash(Data(bytes: __hashSign), for: currentAccount)
+            guard let __txHashSign = __txHashSignData.value  else {
+                self.delegate?.didSendNANJError!(error: "Sign data error")
+                return
+            }
+            
+            let dataR = __txHashSign[..<32]
+            let signR = dataR.hexEncoded
+            print("R: ", signR)
+            
+            let dataS = __txHashSign[32..<64]
+            let signS = dataS.hexEncoded
+            print("S: ", signS)
+            
+            let signV = __txHashSign[64]// + 27
+            print("V: ", signV)
+            
+            
+            //STEP4: SHA3 TX_HASH
+            print("* * * * * * * * * * * * STEP 4 * * * * * * * * * * * *")
+            let __bytes = Array<UInt8>(hex: txHashInput)
+            let __hash = Digest.sha3(__bytes, variant: .keccak256)
+            let txHash = __hash.toHexString().add0x
+            print("txHash: ", txHash)
+            print("\n")
+            
+            //STEP5: CREATE JSON DATA
+            print("* * * * * * * * * * * * STEP 5 * * * * * * * * * * * *")
+            let para:NSMutableDictionary = NSMutableDictionary()
+            para.setValue(forwardEncoded.hexEncoded, forKey: "data")
+            para.setValue(NANJConfig.META_NANJCOIN_MANAGER, forKey: "dest")
+            para.setValue(txHash, forKey: "hash")
+            para.setValue(NANJConfig.PAD, forKey: "nonce")
+            para.setValue(signR, forKey: "r")
+            para.setValue(signS, forKey: "s")
+            para.setValue(signV, forKey: "v")
+            let jsonData = try! JSONSerialization.data(withJSONObject: para, options: JSONSerialization.WritingOptions.init(rawValue: 0))
+            let jsonString = NSString(data: jsonData, encoding: String.Encoding.utf8.rawValue)!
+            print(jsonString)
+            print("\n")
+            
+            //STEP6: PUSH TO API CREATE
+            print("* * * * * * * * * * * * STEP 6 * * * * * * * * * * * *")
+            NANJApiManager.shared.createNANJWallet(params: para) {[weak self] txHash in
+                guard let `self` = self else {return}
+                if txHash == nil || txHash?.count == 0 {
+                    self.delegate?.didSendNANJError?(error: "Send NANJ failed.")
+                } else {
+                    self.delegate?.didSendNANJCompleted?(transaction: NANJTransaction(transaction: ["hash" : txHash!]))
                 }
-                
-                let dataR = __txHashSign[..<32]
-                let signR = dataR.hexEncoded
-                print("R: ", signR)
-                
-                let dataS = __txHashSign[32..<64]
-                let signS = dataS.hexEncoded
-                print("S: ", signS)
-                
-                let signV = __txHashSign[64]// + 27
-                print("V: ", signV)
-                
-                
-                //STEP4: SHA3 TX_HASH
-                print("* * * * * * * * * * * * STEP 4 * * * * * * * * * * * *")
-                let __bytes = Array<UInt8>(hex: txHashInput)
-                let __hash = Digest.sha3(__bytes, variant: .keccak256)
-                let txHash = __hash.toHexString().add0x
-                print("txHash: ", txHash)
-                print("\n")
-                
-                //STEP5: CREATE JSON DATA
-                print("* * * * * * * * * * * * STEP 5 * * * * * * * * * * * *")
-                let para:NSMutableDictionary = NSMutableDictionary()
-                para.setValue(forwardEncoded.hexEncoded, forKey: "data")
-                para.setValue(NANJConfig.META_NANJCOIN_MANAGER, forKey: "dest")
-                para.setValue(txHash, forKey: "hash")
-                para.setValue(NANJConfig.PAD, forKey: "nonce")
-                para.setValue(signR, forKey: "r")
-                para.setValue(signS, forKey: "s")
-                para.setValue(signV, forKey: "v")
-                let jsonData = try! JSONSerialization.data(withJSONObject: para, options: JSONSerialization.WritingOptions.init(rawValue: 0))
-                let jsonString = NSString(data: jsonData, encoding: String.Encoding.utf8.rawValue)!
-                print(jsonString)
-                print("\n")
-                
-                //STEP6: PUSH TO API CREATE
-                    print("* * * * * * * * * * * * STEP 6 * * * * * * * * * * * *")
-                NANJApiManager.shared.createNANJWallet(params: para) {[weak self] txHash in
-                    guard let `self` = self else {return}
-                    if txHash == nil || txHash?.count == 0 {
-                        self.delegate?.didSendNANJError?(error: "Send NANJ failed.")
-                    } else {
-                        self.delegate?.didSendNANJCompleted?(transaction: NANJTransaction(transaction: ["hash" : txHash!]))
-                    }
-                }
-                break
-            case .failure(let error):
-                self.delegate?.didSendNANJError?(error: error.localizedDescription)
-                break
             }
         }
     }
@@ -304,6 +294,23 @@ extension NANJWallet {
                 break
             case .failure(let error):
                 self.delegate?.didGetTransactionList?(transactions: nil)
+                print(error)
+                break
+            }
+        }
+    }
+}
+
+extension NANJWallet {
+    fileprivate func retrieveRelayNonce(completion: @escaping(BigUInt, Error?)-> Void) {
+        let request: RelayNonceRequest = RelayNonceRequest(self.addressETH)
+        Session.shared.send(request) { result in
+            switch result {
+            case .success(let nonce):
+                completion(nonce,nil)
+                break
+            case .failure(let error):
+                completion(0, error)
                 print(error)
                 break
             }
